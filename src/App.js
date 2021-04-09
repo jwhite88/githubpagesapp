bc52844b2bb6ccedfb0c9bc1bca9f061be76a55c
jwhite88 <jwhite789@protonmail.com>
2021-04-09 13:44:58 -0400

    Added basic React Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Projects from './components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
+// Multiple 'Route' tags must always be wrapped
+// in a 'div' tag OR a 'Switch' tag.
   );
 }
 
